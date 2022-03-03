e221c7be9e1db2933e074995033d63149b254c60
Allan <Allan@DESKTOP-T9MTMN8>
2022-03-03 14:09:45 -0300

    First commit

---
+++ src/com/dio/MeuProgramaJava.java
@@ -0,0 +1,18 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class MeuProgramaJava {
+
+	public static void main(String[] args) {
+		
+	Gato gato = new Gato();
+	
+	System.out.print(gato);
+	
+	}
+		
+		/*	System.out.print("Hello World!");
+	}*/
+
+}
